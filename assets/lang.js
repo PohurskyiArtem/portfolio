4e75d92d29a0c8727e9344dadf1025de7c78e622
Pohurskyi Artem <46791938+PohurskyiArtem@users.noreply.github.com>
2021-05-16 13:18:36 +0300

    portfolio ...

---
+++ assets/lang.js
@@ -0,0 +1,107 @@
+const langArr = {
+    "greeting-top": {
+        "ru": "Привет, меня зовут Артём и я",
+        "en": "Hello, my name is Artem and I am",
+        "ua": "Привіт, мене звати Артем і я"
+    },
+    "main-title": {
+        "ru": "Full Stack веб-разработчик",
+        "en": "Full Stack Web Developer",
+        "ua": "Full Stack веб-розробник"
+    },
+    "greeting-bottom": {
+        "ru": "я создаю современные, адаптивные и оптимизированные веб-приложения",
+        "en": "creating modern and responsive Web Application, Web Pages",
+        "ua": "я створюю сучасні, адаптивні та оптимізовані веб-додатки"
+    },
+    "nav-home": {
+        "ru": "Главная",
+        "en": "Home",
+        "ua": "Головна"
+    },
+    "nav-services": {
+        "ru": "Услуги",
+        "en": "Services",
+        "ua": "Послуги"
+    },
+    "nav-works": {
+        "ru": "Портфолио",
+        "en": "Works",
+        "ua": "Портфоліо"
+    },
+    "nav-skills": {
+        "ru": "Навыки",
+        "en": "Skills",
+        "ua": "Навички"
+    },
+    "nav-testimonials": {
+        "ru": "Отзывы",
+        "en": "Testimonials",
+        "ua": "Відгуки"
+    },
+    "nav-contact": {
+        "ru": "Контакты",
+        "en": "Contact",
+        "ua": "Контакти"
+    },
+    "specialty": {
+        "ru": "Чем я занимаюсь",
+        "en": "This is what I do",
+        "ua": "Чим я займаюсь"
+    },
+    "quote1": {
+        "ru": "Большинство хороших программистов делают свою работу не потому, что ожидают оплаты или признания, а потому что получают удовольствие от программирования",
+        "en": "Most good programmers do their job not because they expect payment or recognition, but because they enjoy programming",
+        "ua": "Більшість хороших програмістів роблять свою роботу не тому, що очікують оплати або визнання, а тому що отримують задоволення від програмування"
+    },
+    "quote2": {
+        "ru": "Сделать то, что нужно, так, как нужно, тогда, когда нужно и сделать это за один раз – вот что такое «профессионализм",
+        "en": "To do what is needed, the way it is necessary, when it is necessary, and to do it in one go - this is what “professionalism” is.",
+        "ua": "Зробити те, що потрібно, як потрібно, тоді, коли потрібно і зробити це за один раз - ось що таке \"професіоналізм\""
+    },
+    "spec-title1": {
+        "ru": "Вёрстка",
+        "en": "Layout",
+        "ua": "Верстка"
+    },
+    "spec-title2": {
+        "ru": "Одностраничные приложения",
+        "en": "Single Page Application",
+        "ua": "Односторінкові додатки"
+    },
+    "spec-title3": {
+        "ru": "Wordpress",
+        "en": "Wordpress",
+        "ua": "Wordpress"
+    },
+    "spec-title4": {
+        "ru": "text",
+        "en": "text",
+        "ua": "text"
+    },
+    "spec-descr1": {
+        "ru": "Валидная, кроссбраузерная, адаптивная и оптимизированная вёрстка лендингов, многостраничных сайтов и интернет-магазинов",
+        "en": "Valid, cross-browser, adaptive and optimized layout of landing pages, multi-page sites and online stores",
+        "ua": "Валідна, кросбраузерна, адаптивна та оптимізована верстка лендингів, багатосторінкових сайтів і інтернет-магазинів"
+    },
+    "spec-descr2": {
+        "ru": "Разработка одностаничных приложений и панелей администратора на React.js",
+        "en": "Development of single page applications and admin panels on React.js ",
+        "ua": "Розробка односторінкових додатків та панелей адміністратора на React.js"
+    },
+    "spec-descr3": {
+        "ru": "Создание или интеграция готовых веб-сайтов в CMS Wordpress",
+        "en": "Creation or integration into CMS Wordpress",
+        "ua": "Створення та інтегрування готових веб-сайтів в CMS Wordpress"
+    },
+    "spec-descr4": {
+        "ru": "text",
+        "en": "text",
+        "ua": "text"
+    },
+    "portfolio": {
+        "ru": "Мои работы",
+        "en": "My Works",
+        "ua": "Мої роботи"
+    }
+}
